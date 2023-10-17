995e6d259327c1a7c1b76dfe42a8b308529e24c5
Alien Brain <murad.dub@gmail.com>
2023-10-17 07:02:52 +0600

    hudai

--- index.js
+++ index.js
@@ -1,61 +1,9 @@
-// const express = require("express");
-// const app = require();
-
-// const cors = require("cors");
-
-// const port = process.env.port || 5000;
-
-// // Middleware
-// app.use(express.json());
-// app.use(cors());
-
-// //devalienbrain
-// // MJp6Sr08W2KFFY8R;
-
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const uri =
-//   "mongodb+srv://devalienbrain:MJp6Sr08W2KFFY8R@cluster0.m38robg.mongodb.net/?retryWrites=true&w=majority";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// app.get("/", (req, res) => {
-//   res.send("Simple CRUD MongoDB Starts!");
-// });
-
-// app.listen(port, () => {
-//   console.log("Hello! MongoDB CRUD Operation Starts..");
-// });
-
-
 const express = require("express");
-const app = express(); // Add 'express' to create an Express application
+const app = require();
 
 const cors = require("cors");
 
-const port = process.env.PORT || 5000; // Change 'port' to 'PORT' for the environment variable
+const port = process.env.port || 5000;
 
 // Middleware
 app.use(express.json());
@@ -94,9 +42,60 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Simple CRUD MongoDB Starts!"); // Corrected 'req' to 'res'
+  res.send("Simple CRUD MongoDB Starts!");
 });
 
 app.listen(port, () => {
   console.log("Hello! MongoDB CRUD Operation Starts..");
 });
+
+// const express = require("express");
+// const app = express(); // Add 'express' to create an Express application
+
+// const cors = require("cors");
+
+// const port = process.env.PORT || 5000; // Change 'port' to 'PORT' for the environment variable
+
+// // Middleware
+// app.use(express.json());
+// app.use(cors());
+
+// //devalienbrain
+// // MJp6Sr08W2KFFY8R;
+
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri =
+//   "mongodb+srv://devalienbrain:MJp6Sr08W2KFFY8R@cluster0.m38robg.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+// app.get("/", (req, res) => {
+//   res.send("Simple CRUD MongoDB Starts!"); // Corrected 'req' to 'res'
+// });
+
+// app.listen(port, () => {
+//   console.log("Hello! MongoDB CRUD Operation Starts..");
+// });
